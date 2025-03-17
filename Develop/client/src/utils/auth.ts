ec7fdbcb3278fee788610bc44766b2d5961b4933
Paige Feldmann <paigeafeldmann@gmail.com>
2025-03-17 15:46:07 -0500

    recent changes

---
+++ Develop/client/src/utils/auth.ts
@@ -0,0 +1,48 @@
+import { JwtPayload, jwtDecode } from "jwt-decode";
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+    return jwtDecode(this.getToken());
+  }
+
+  loggedIn() {
+    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token: string) {
+    try {
+      // TODO: return a value that indicates if the token is expired
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken(): string {
+    // TODO: return the token
+    const loggedUser = localStorage.getItem("id_token") || "";
+    return loggedUser;
+  }
+
+  login(idToken: string) {
+    // TODO: set the token to localStorage
+    localStorage.setItem("id_token", idToken);
+    // TODO: redirect to the home page
+    window.location.assign("/");
+  }
+
+  logout() {
+    // TODO: remove the token from localStorage
+    localStorage.removeItem("id_token");
+    // TODO: redirect to the login page
+    window.location.assign("/");
+  }
+}
+
+export default new AuthService();
