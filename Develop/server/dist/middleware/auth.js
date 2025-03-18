77bce48b1c2066e5174e9de7028aed1c3943d59a
Paige Feldmann <paigeafeldmann@gmail.com>
2025-03-18 14:47:29 -0500

    adding minor changes for render deployment

--- Develop/server/dist/middleware/auth.js
+++ Develop/server/dist/middleware/auth.js
@@ -4,7 +4,7 @@ export const authenticateToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
         const token = authHeader.split(' ')[1];
-        const secretKey = process.env.JWT_SECRET_KEY || "";
+        const secretKey = process.env.JWT_SECRET_KEY || "secret";
         jwt.verify(token, secretKey, (err, user) => {
             if (err) {
                 return res.sendStatus(403);
